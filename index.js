355eda394b4e9faa9375ff4e2ee8dca85dbae4e6
Max Sealey <maxsealey@users.noreply.github.com>
2022-01-13 17:24:17 -0700

    solution

--- index.js
+++ index.js
@@ -11,6 +11,27 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() { 
+  // const arr = tutorials.map(function (arrayItemToBeSplit) {
+  //   return arrayItemToBeSplit.split(" ")
+  // })
+
+  const arr = tutorials.map((arrayItemToBeSplit) => arrayItemToBeSplit.split(" "))
+  
+  const arr2 = arr.map(newParameter => newParameter.map(str => {
+    return str.charAt(0).toUpperCase() + str.slice(1) 
+  }).join(' '))
+//
+  // .toUpperCase() + newParameter.slice(1)
+
+    // takes in a parameter of an array (each element of arr)
+    // To each element in arr, do what?
+
+    // return each element to be assigned to arr2 after being modified
+    console.log(arr2)
+    return arr2
 }
+
+titleCased(tutorials); // function call - tells the program, do this
+// whatever is passed into the function call is the argument
+
